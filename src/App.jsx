5d9428a50233e6f7cafedf112ad7666adde09adc
Rashmi Singh <rasmi2114@gmail.com>
2026-08-17 14:21:00 +0200

    Sumary

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ export const App = () => {
       <h1>Awarapan 2</h1>
 
       <h3>Rating: 7.3/10 </h3>
-
-      <p>
-      Shivam returns to the crime world, where his path forward demands redemption, love, and sacrifice. 
-       <br />As relationships deepen and conflicts intensify, every choice tests his resolve and shapes his destiny.
-      </p>
-      <div class="details">
+       <div class="details">
       <p>Director : Nitin Kakkar</p>
       <p>Writers  : Vishesh BhattSanjay MasoommBilal Siddiqi</p>
       <p>Stars    : Emraan HashmiShabana AzmiDisha Patani</p>
       <p>See production info at IMDbPro </p>
       </div>
-      
+      <p>
+      Sumary : Shivam returns to the crime world, where his path forward demands redemption, love, and sacrifice. 
+       <br />As relationships deepen and conflicts intensify, every choice tests his resolve and shapes his destiny.
+      </p>
+     
+
     </div>
   );
 };

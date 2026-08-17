ba8c1633cd3381d39e2e382547095c5a5e0b7a8a
Rashmi Singh <rasmi2114@gmail.com>
2026-08-17 14:44:01 +0200

    details

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 export const App = () => {
   return (
     <div>
+     <MovieReating/>
+     <MovieReating/>
+     <MovieReating/>
+     <MovieReating/>
+    </div>
+  );
+};
+
+const MovieReating = () => {
+   return (
+    <div style={{ borderBottom: "1px solid #393737" , margin:"1rem" }}>
       <div>
-        <img src="images/movie.jpg" alt="movie" width="40%" height="40%" />
+        <img src="images/movie.jpg" alt="movie" width="20%" height="20%" />
       </div>
 
       <h1>Awarapan 2</h1>
 
       <h3>Rating: 7.3/10 </h3>
-       <div class="details">
-      <p>Director : Nitin Kakkar</p>
-      <p>Writers  : Vishesh BhattSanjay MasoommBilal Siddiqi</p>
-      <p>Stars    : Emraan HashmiShabana AzmiDisha Patani</p>
-      <p>See production info at IMDbPro </p>
+      <div class="details">
+        <p>Director : Nitin Kakkar</p>
+        <p>Writers  : Vishesh BhattSanjay MasoommBilal Siddiqi</p>
+        <p>Stars    : Emraan HashmiShabana AzmiDisha Patani</p>
+        <p>See production info at IMDbPro </p>
       </div>
-      <p>
+      <p style={{marginBottom:"1rem" }}>
       Sumary : Shivam returns to the crime world, where his path forward demands redemption, love, and sacrifice. 
        <br />As relationships deepen and conflicts intensify, every choice tests his resolve and shapes his destiny.
       </p>
-     
-
     </div>
-  );
+   );
+
 };

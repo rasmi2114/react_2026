e313d2b14f1d9b79869a7fea5af3e6c9aee358e8
Rashmi Singh <rasmi2114@gmail.com>
2026-08-17 13:02:53 +0200

    my app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
 export const App = () => {
-  return <h1> Welcome to React! </h1>
+  return (
+    <div>
+      <div>
+        <img src="images/movie.jpg" alt="movie" width="40%" height="40%" />
+      </div>
 
+      <h1>Awarapan 2</h1>
+
+      <h3>Rating: 7.3/10 </h3>
+
+      <p>
+      Shivam returns to the crime world, where his path forward demands redemption, love, and sacrifice. 
+       <br />As relationships deepen and conflicts intensify, every choice tests his resolve and shapes his destiny.
+      </p>
+      <div class="details">
+      <p>Director : Nitin Kakkar</p>
+      <p>Writers  : Vishesh BhattSanjay MasoommBilal Siddiqi</p>
+      <p>Stars    : Emraan HashmiShabana AzmiDisha Patani</p>
+      <p>See production info at IMDbPro </p>
+      </div>
+    </div>
+  );
 };

720e94f5cd40ed59985fe12b3debe66f7eecb2cf
Rashmi Singh <rasmi2114@gmail.com>
2026-08-28 12:42:47 +0200

    click

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // import { Fragment } from "react";
-import MovieRating from "./components/MovieRating/MovieRating";
+import { EventHandling } from "./components/EventHandling/EventHandling";
+// import MovieRating from "./components/MovieRating/MovieRating";
+
 import "./components/MovieRating/Movies.css"
 
 export const App = () => {
   //const type="Romantic";
   return (
     < section className="container">
-      <h1 className="card-heading">List Of Upcoming Released Movies</h1>
-      <MovieRating  />
+      {/* <h1 className="card-heading">List Of Upcoming Released Movies</h1> */}
+      {/* <MovieRating  /> */}
+      <EventHandling />
     </section>
     
   );
--- src/components/EventHandling/EventHandling.jsx
+++ src/components/EventHandling/EventHandling.jsx
@@ -1,4 +1,16 @@
 import "../EventHandling/EventHandling.css";
+
+function handleButtonClick() {
+    alert("Hi, Im on button click evenet");
+
+}
+
 export const EventHandling =() => {
+    return(
+        <>
+        <button onClick={handleButtonClick}>Click me</button>
+        </>
+    )
+
 
 }

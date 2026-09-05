c3f3d192c8f08c4cb63ea86ffc62162bb5bbaac2
Rashmi Singh <rasmi2114@gmail.com>
2026-09-05 13:24:01 +0200

    appfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 
-import "./projects/ToDoApp/todo.css";
-import { Todo } from "./projects/ToDoApp/Todo";
-// import { ToggleSwitch } from "./projects/ToggleSwitch/ToggleSwtich";
-// import { State } from "./components/hooks/State";
-// import { Fragment } from "react";
-// import { EventHandling } from "./components/EventHandling/EventHandling";
-// import MovieRating from "./components/MovieRating/MovieRating";
-// import { EventProps } from "./components/EventHandling/Eventprops";
-// import { EventPropagation } from "./components/EventHandling/EventPropagation";
+import MyTodo from "./projects/myTodoApp/MyTodo";
+import "./projects/myTodoApp/myTodo.css";
+// import "./projects/ToDoApp/todo.css";
+
 
 
 export const App = () => {
   //const type="Romantic";
   return (
     < section className="container">
-      {/* <h1 className="card-heading">List Of Upcoming Released Movies</h1> */}
-      {/* <MovieRating  /> */}
-      {/* <EventHandling /> */}
-      {/* <EventProps/> */}
-      {/* < EventPropagation /> */}
-      {/* < State/> */}
-      {/* <LiftingState /> */}
-      {/* <ToggleSwitch /> */}
-      {/* <DarkMode /> */}
-      <Todo/>
+      <MyTodo />
     </section>
     
   );

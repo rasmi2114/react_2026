6dd471df33c420401a399001d81f019df5d105e8
Rashmi Singh <rasmi2114@gmail.com>
2026-09-09 11:26:39 +0200

    form

--- src/components/hooks/UseState/Registration.jsx
+++ src/components/hooks/UseState/Registration.jsx
@@ -22,7 +22,7 @@ export  const Registration = () => {
     return (
         <>
         <form onSubmit={handelFormSubmit}>
-            <div className="container">
+            <div className="container-reg">
                 <h1>Sign Up</h1>
                   <p>Please fill in this form to create an account.</p>
 
@@ -36,8 +36,79 @@ export  const Registration = () => {
                   value={user.firstName}
                   onChange={handelInputchange}
                   />
+
+                  
+                <label htmlFor="lastName">
+                    <b>Last Name</b>
+                </label>
+                <input
+                    type="text"
+                    name="lastName"
+                    placeholder="Enter lastName"
+                    required
+                    value={user.lastName}
+                    onChange={handelInputchange}
+                />
+                 <label htmlFor="email">
+                    <b>Email</b>
+                </label>
+                <input
+                    type="text"
+                    placeholder="Enter Email"
+                    name="email"
+                    required
+                    value={user.email}
+                    onChange={handelInputchange}
+                />
+
+                <label htmlFor="password">
+                    <b>Password</b>
+                </label>
+                <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                    required
+                    value={user.password}
+                    onChange={handelInputchange}
+                />
+
+                <label htmlFor="phone">
+                    <b>Phone Number</b>
+                </label>
+
+                <input
+                    type="phone"
+                    name="phoneNumber"
+                    placeholder="9876543211"
+                    required
+                    value={user.phoneNumber}
+                    onChange={handelInputchange}
+                />
+
+                <p>
+                    By creating an account you agree to our
+                    <a href="#" style={{ color: "dodgerblue" }}>
+                    Terms & Privacy
+                    </a>
+                </p>
+
+                <div className="clearfix">
+                    <button type="submit" className="btn">
+                    Sign Up
+                    </button>
+                </div>
             </div>
         </form>
+            <section className="summary"
+                style={{ textAlign: "center", marginTop: "30px" }}>
+                <p>Hello, my name is
+                <span>
+                    {user.firstName} {user.lastName}
+                </span>. My email address is <span>{user.email}</span> and my phone number is
+                <span>{user.phoneNumber}</span>.
+                </p>
+            </section>
         </>
     );
 

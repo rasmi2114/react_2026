c58c34c6b7753cbd2c94cd02256e2ab9d8a34c43
Rashmi Singh <rasmi2114@gmail.com>
2026-09-18 14:33:40 +0200

    ShortCircuit

--- src/projects/PokemonApp/PokemonApp.jsx
+++ src/projects/PokemonApp/PokemonApp.jsx
@@ -1,60 +1,92 @@
-import "./Pokemon.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PokemonCards } from "./PokemonCards";
+import "../../components/hooks/UseEffect/Pokemon.css";
 
-export const PokemonApp =() => {
+export const PokemonApp = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [search, setSearch] = useState("");
 
   const API = "https://pokeapi.co/api/v2/pokemon?limit=124";
+  // subscribe to thapa technical youtube channel: https://www.youtube.com/thapatechnical
 
-  const fetchPokemon =async () => {
+  const fetchPokemon = async () => {
     try {
-        const res = await fetch(API);
+      const res = await fetch(API);
+      const data = await res.json();
+      //   console.log(data);
+
+      const detailedPokemonData = data.results.map(async (curPokemon) => {
+        const res = await fetch(curPokemon.url);
         const data = await res.json();
-        //console.log(data);
+        return data;
+      });
+      //   console.log(detailedPokemonData);
 
-        const detailedPokemonData  = data.result.map(async(curPokemon) => {
-            const res = await fetch(curPokemon.url);
-            const data = await res.json();
-            return data;
-        });
-          //console.log(detailedPokemonData);
+      const detailedResponses = await Promise.all(detailedPokemonData);
+      console.log(detailedResponses);
+      setPokemon(detailedResponses);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error);
+    }
+  };
 
-          const detailedResponses = await Promise.all(detailedPokemonData);
-          console.log(detailedResponses);
-          setPokemon(detailedResponses);
-          setLoading(false);
-        }catch (error) {
-            console.log(error);
-            setLoading(false);
-            setError(error);
-        }
-   };
-
-   useEffect(() => {
+  useEffect(() => {
     fetchPokemon();
-   }, []);
+  }, []);
 
-   //Loading 
-   if (loading) {
+  //search functionality
+
+  const searchData = pokemon.filter((curPokemon) =>
+    curPokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (loading) {
     return (
-        <div>
-            <h1>Loading...</h1>
-        </div>
+      <div>
+        <h1>Loading....</h1>
+      </div>
     );
-   }
+  }
 
-   //error 
-   if (error) {
+  if (error) {
     return (
-        <div>
-            <h1>{error.message}</h1>
-        </div>
+      <div>
+        <h1>{error.message}</h1>
+      </div>
     );
-   }
+  }
 
-    return ();
+
+  return (
+    <>
+      <section className="container">
+        <header>
+          <h1> Lets Catch Pokémon</h1>
+        </header>
+        <div className="pokemon-search">
+          <input
+            type="text"
+            placeholder="search Pokemon"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+        <div>
+          <ul className="cards">
+            {/* {pokemon.map((curPokemon) => { */}
+            {searchData.map((curPokemon) => {
+              return (
+                <PokemonCards key={curPokemon.id} pokemonData={curPokemon} />
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 };
--- src/projects/PokemonApp/PokemonCards.jsx
+++ src/projects/PokemonApp/PokemonCards.jsx
@@ -1,3 +1,13 @@
-export const PokemonCards = () => {
-    return();
+export const PokemonCards = ({pokemonData}) => {
+    return(
+        <li className="pokemon-card">
+         <figure>
+            <img 
+                src={pokemonData.sprites.other.dream_world.front_default}
+                alt={pokemonData.name}
+                className="pokemon-image"
+            />
+         </figure>
+        </li>
+    );
 };

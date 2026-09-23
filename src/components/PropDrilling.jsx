1ebe361db31c101e9124d4ca3b5a0e639cbcacf5
Rashmi Singh <rasmi2114@gmail.com>
2026-09-23 11:41:38 +0200

    drilling

---
+++ src/components/PropDrilling.jsx
@@ -0,0 +1,35 @@
+export const ParentComponent  = () => {
+     return (
+    <section>
+      <h1>Component A</h1>
+      <ChildComponent data="React.js"/>
+    </section>
+  );
+
+};
+
+const ChildComponent = (props) =>{
+    return (
+        <>
+        <h1>Hello, I am component B </h1>
+        <GrandChildComponent data={props.data}/>
+        </>
+    );
+};
+
+const GrandChildComponent = (props) =>{
+    return (
+        <>
+        <h1>Hello, I am component C </h1>
+        <GrandGrandChildComponent data={props.data} />
+        </>
+    );
+};
+
+const GrandGrandChildComponent = (props) =>{
+    return (
+        <>
+        <h1>Hello, I Love {props.data} </h1>
+        </>
+    );
+};

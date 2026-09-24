4244801ef7c7274a9c71676340f80128ccd6c06f
Rashmi Singh <rasmi2114@gmail.com>
2026-09-24 10:56:31 +0200

    handleToggleTheme

--- src/components/hooks/ContextAPI/DarkLight.jsx
+++ src/components/hooks/ContextAPI/DarkLight.jsx
@@ -1,11 +1,29 @@
-import { Children, createContext, useState } from "react";
+import { Children, createContext, use, useState } from "react";
 
-export const DarkLight = createContext();
+export const ThemeContext = createContext();
 
-export const DarkProvider = ({Children}) => {
+export const ThemeProvider = ({Children}) => {
     const [theme, setTheme ] = useState ('dark');
 
-    return <DarkLight.Provider value={{theme}}>{Children}</DarkLight.Provider>
+    const handleToggleTheme =() => {
+        return setTheme((prevThem) => (prevThem == "dark" ? "light" : "dark"));
+    }
 
+    return <DarkLight.Provider value={{theme, handleToggleTheme}}>{Children}</DarkLight.Provider>
+
+};
+
+//creating a component
+
+export const DarkLight =() => {
+    const {theme, handleToggleTheme} = use(ThemeContext);
+
+    return (
+        <div>
+            <h1>Dark Light Mode Website</h1>
+            <p>Hello!! My React v19 Fans 👍</p><br></br>
+            <button onClick={handleToggleTheme}> {theme === "dark" ? "Switch to light mode" : "Switch to Dark mode"} </button>
+        </div>
+    );
 };
 

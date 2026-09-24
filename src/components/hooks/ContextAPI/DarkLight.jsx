720266f8cba228d5dcf6267abc1c0ee815659582
Rashmi Singh <rasmi2114@gmail.com>
2026-09-24 10:34:04 +0200

    DarkLight

---
+++ src/components/hooks/ContextAPI/DarkLight.jsx
@@ -0,0 +1,7 @@
+import { Children, createContext, useState } from "react";
+
+export const DarkLight =createContext();
+export const DarkProvider = ({Children}) => {
+    const [theme, setTheme ] = useState ('dark');
+
+};
